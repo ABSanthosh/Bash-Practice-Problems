c70b1c041fa23cd3251704d5d51cabe3e569114c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-23 23:32:12 +0530

    feat(Answer): Q22

---
+++ q22.sh
@@ -0,0 +1,8 @@
+# Write an automated script that reboots the system 5 time, before perfect login in to the   
+#   User account
+#   Note: to write the above script you need administrative privilege.
+
+
+#!/bin/sh
+
+
